323f04841375e013a1a7c85a29128377bab10589
abm <junited@163.com>
2024-11-13 19:52:02 +0800

    2024/11/13 完成了打包

--- python/main.py
+++ python/main.py
@@ -3,6 +3,8 @@ import EVT
 import CartesianProductCal
 import ScriptGenerator
 import os
+import sys
+import importlib.util
 
 def main():
     # Step 1: Create the Dictionaries for Event and Condition
@@ -21,10 +23,21 @@ def main():
 
     # Step 4: Generate requirement verifier script
     ScriptGenerator.main()
-
+    print("requirement_verifier.py generated successfully.")
+    input()
     # Step 5: Run the script to verify the requirements
-    imported_module = __import__('requirement_verifier')
+
+    current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+    script_path = os.path.join(current_dir, 'requirement_verifier.py')
+
+    spec = importlib.util.spec_from_file_location('requirement_verifier', script_path)
+    imported_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(imported_module)
+
     imported_module.main()
+
+
 
 if __name__ == '__main__':
     main()
