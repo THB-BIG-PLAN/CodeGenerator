2914172dfad3acb39614298ea6ffd49ca6271a8d
abm <junited@163.com>
2024-11-13 10:05:43 +0800

    2024/11/13 备份，换一种思路试试

--- python/requirement_verifier_test.py
+++ python/requirement_verifier_test.py
@@ -16,3 +16,12 @@ class State:
         self.current_state = {'BdcSeedsignal': BdcSeedsignal, 'BdcWlcmsignal': BdcWlcmsignal, 'DLC_u8TurnLightTwice': DLC_u8TurnLightTwice, 
                 'EEP_LOGO_ENABLE_FLAG': EEP_LOGO_ENABLE_FLAG, 'EspAutoHoldActvSts': EspAutoHoldActvSts, 'PLB_u8LBSts': PLB_u8LBSts, 
                 'PPL_boolPosnLampSts': PPL_boolPosnLampSts, 'PRM_u8PowerSts': PRM_u8PowerSts, 'VcuGearPosn': VcuGearPosn, 'TIMEFLAGNUM': 0}
+        self.previous_state = {k: 0 for k in self.current_state.keys()}
+        if(((self.current_state['DLC_u8TurnLightTwice']!= self.previous_state['DLC_u8TurnLightTwice']))) and ((self.current_state['DLC_u8TurnLightTwice']!= self.previous_state['DLC_u8TurnLightTwice'])):
+            self.current_state['TIMEFLAGNUM'] += 1
+        if(((self.current_state['PRM_u8PowerSts']!= self.previous_state['PRM_u8PowerSts']) and (self.current_state['PRM_u8PowerSts'] == 1)or ((self.current_state['PRM_u8PowerSts']!= self.previous_state['PRM_u8PowerSts']) and (self.current_state['PRM_u8PowerSts'] == 0))) and ((self.current_state['EEP_LOGO_ENABLE_FLAG'] == 1)):
+            self.current_state['TIMEFLAGNUM'] += 1
+        if(((self.current_state['EEP_LOGO_ENABLE_FLAG']!= self.previous_state['EEP_LOGO_ENABLE_FLAG']) and (self.current_state['EEP_LOGO_ENABLE_FLAG'] == 0))) and ((self.current_state['PRM_u8PowerSts'] == 2)):
+            self.current_state['TIMEFLAGNUM'] += 1
+        if(((self.current_state['BdcSeedsignal']!= self.previous_state['BdcSeedsignal']) and (self.current_state['BdcSeedsignal'] == 0))) and ((self.current_state['BdcSeedsignal']!= self.previous_state['BdcSeedsignal']) and (self.current_state['BdcSeedsignal'] == 0)):
+            self.current_state['TIMEFLAGNUM'] += 1
--- python/scriptdemo.py
+++ python/scriptdemo.py
@@ -5,8 +5,6 @@ import re
 
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-
-
 CONFIG_FILE = "config.xlsx"
 WRITE_FILE = "requirement_verifier_test.py"
 HEADER = '''
@@ -58,7 +56,6 @@ class State:
     def __init__(self, {SignalString}):
         self.current_state = {{{StateString}}}
         self.previous_state = {{k: 0 for k in self.current_state.keys()}}
-        
 ''')
 
 # 增加是否有TIMEFLAGNUM的判断
@@ -66,6 +63,59 @@ EventDataFrame = pd.read_excel(CONFIG_FILE, sheet_name=4)
 ConditionDataFrame = pd.read_excel(CONFIG_FILE, sheet_name=2)
 AddTimeString = ''
 for i, row in EventDataFrame.iterrows():
-    if re.search('addTimer',row.iloc[1]) is not None or re.search('addTimer',row.iloc[2]) is not None:
-
-
+    if (pd.notna(row.iloc[1]) and re.search('addTimer', row.iloc[1]) is not None) or (
+            pd.notna(row.iloc[2]) and re.search('addTimer', str(row.iloc[2])) is not None):
+        row_value = row.iloc[0]
+        ConditionList = ConditionDataFrame[ConditionDataFrame.iloc[:, 3].astype(str) == str(row_value)].index.tolist()
+        ConditionString = '        if(('
+        for j in range(len(ConditionList)):
+            ConditionSeries = ConditionDataFrame.iloc[ConditionList[j]]
+            Symbol = ConditionSeries.iloc[1]
+            ConditionString += f"(self.current_state['{ConditionSeries.iloc[0]}']"
+            if Symbol == 'EQ':
+                ConditionString += f" == {ConditionSeries.iloc[2]})"
+            elif Symbol == 'NEQ':
+                ConditionString += f"!= {ConditionSeries.iloc[2]})"
+            elif Symbol == 'CHANGE':
+                ConditionString += f"!= self.previous_state['{ConditionSeries.iloc[0]}'])"
+            elif Symbol == 'CHANGETO':
+                ConditionString += f"!= self.previous_state['{ConditionSeries.iloc[0]}']) and (self.current_state['{ConditionSeries.iloc[0]}'] == {ConditionSeries.iloc[2]})"
+            elif Symbol == 'GREATER':
+                ConditionString += f" > {ConditionSeries.iloc[2]})"
+            elif Symbol == 'GREATEROREQ':
+                ConditionString += f" >= {ConditionSeries.iloc[2]})"
+            elif Symbol == 'LESS':
+                ConditionString += f" < {ConditionSeries.iloc[2]})"
+            elif Symbol == 'LESSOREQ':
+                ConditionString += f" <= {ConditionSeries.iloc[2]})"
+            if len(ConditionList) > 1 and j < len(ConditionList) - 1:
+                ConditionString += 'or ('
+        ConditionString += '))'
+        if pd.notna(row.iloc[3]):
+            ConditionString += ' and ('
+        for j in range(3, row.shape[0]):
+            if pd.notna(row.iloc[j]) and row.iloc[j] != 'AND':
+                row_value = row.iloc[j]
+                ConditionList = ConditionDataFrame[ConditionDataFrame.iloc[:, 4].astype(str) == str(row_value)].index.tolist()
+                ConditionSeries = ConditionDataFrame.iloc[ConditionList[0]]
+                Symbol = ConditionSeries.iloc[1]
+                ConditionString += f"(self.current_state['{ConditionSeries.iloc[0]}']"
+                if Symbol == 'EQ':
+                    ConditionString += f" == {ConditionSeries.iloc[2]})"
+                elif Symbol == 'NEQ':
+                    ConditionString += f"!= {ConditionSeries.iloc[2]})"
+                elif Symbol == 'CHANGE':
+                    ConditionString += f"!= self.previous_state['{ConditionSeries.iloc[0]}'])"
+                elif Symbol == 'CHANGETO':
+                    ConditionString += f"!= self.previous_state['{ConditionSeries.iloc[0]}']) and (self.current_state['{ConditionSeries.iloc[0]}'] == {ConditionSeries.iloc[2]})"
+                elif Symbol == 'GREATER':
+                    ConditionString += f" > {ConditionSeries.iloc[2]})"
+                elif Symbol == 'GREATEROREQ':
+                    ConditionString += f" >= {ConditionSeries.iloc[2]})"
+                elif Symbol == 'LESS':
+                    ConditionString += f" < {ConditionSeries.iloc[2]})"
+                elif Symbol == 'LESSOREQ':
+                    ConditionString += f" <= {ConditionSeries.iloc[2]})"
+        ConditionString += "):\n            self.current_state['TIMEFLAGNUM'] += 1\n"
+        AddTimeString += ConditionString
+RequirementVerifierFile.write(AddTimeString)
