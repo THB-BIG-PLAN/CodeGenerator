c14e1575648ffd4bfc843b7d4696604845d0185e
MasterCat520 <623787460@qq.com>
2024-12-05 14:51:33 +0800

    2024/12/05 新分支

--- python/test.py
+++ python/test.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import Exception
-
+import re
 
 def main():
-    InitDataFrame = pd.read_excel('config.xlsx', sheet_name='Init')
-    InitString = ''
-    for index, row in InitDataFrame.iterrows():
-        if pd.isna(row.loc['Type']) or row.loc['Type'] == '':
-            raise Exception.EmptyTypeError(index)
-        if pd.isna(row.loc['Value']) or row.loc['Value'] == '':
-            raise Exception.EmptyValueError(index)
-        InitString += f"    Set_{row.loc['Type']}_{row.loc['SignalName']}({row.loc['Value']})\n"
-    print(InitString)
-
+    s = input("Enter a string: ")
+    pattern = r'\((.*),(.*)\)'
+    result = re.search(pattern, s)
+    if result:
+        x = result.group(1)
+        y = result.group(2)
+        try:
+            x = int(result.group(1))
+        except ValueError:
+            pass
+        try:
+            y = int(result.group(2))
+        except ValueError:
+            pass
+        
+        print("x =", x)
+        print("y =", y)
+    else:
+        print("No match")
 
 if __name__ == '__main__':
     main()
